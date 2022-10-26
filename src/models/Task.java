f27831dd906ec35c55a0c2ed9b737caaa775be8a
haruki.aragane <haruki.06.04a@gmail.com>
2022-10-26 15:07:49 +0900

    new.servlet/indexservletの修正, layoutの追加

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,11 +7,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 
 @Entity
-@Table(name="tasklist")
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query="select t from Task as t order by t.id desc"
+    )
+})
+@Table(name="tasks")
 public class Task {
     @Id
     @Column(name="id")
